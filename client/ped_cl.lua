3f45c5acd0ec2dce8f7fa2735807a59907e56874
visibait <33099726+visibait@users.noreply.github.com>
2021-05-05 17:50:46 +0200

    Optimized (Not tested) (#1)
    * Optimized (Not tested)
    
    * RFR: Revert and remove citizen
    
    * RFR: Removed Citizen
    
    * RFR: Globbing
    
    Co-authored-by: Bombay <72895600+XiosBombay@users.noreply.github.com>

--- client/ped_cl.lua
+++ client/ped_cl.lua
@@ -1,21 +1,21 @@
-Citizen.CreateThread(function()
+local isArmed = false
+
+CreateThread(function()
 	while Config.DropWeapons do
-		Citizen.Wait(1000)
+		Wait(1000)
 		NoWeapons()
 	end
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while Config.Actions do
-        Citizen.Wait(5)
+        Wait(5)
         local ped = PlayerPedId()
-
         if not IsControlPressed(0, 330) or not IsControlPressed(0, 331) then
             DisablePlayerFiring(ped, true)
             DisableControlAction(0, 142, true)
         end
-
-        if IsPedArmed(ped, 6) then
+        if isArmed then
             DisableControlAction(1, 140, true)
             DisableControlAction(1, 141, true)
             DisableControlAction(1, 142, true)
@@ -23,7 +23,20 @@ Citizen.CreateThread(function()
     end
 end)
 
--- No weapons from peds die
+CreateThread(function()
+    while Config.Actions do
+        Wait(200) -- You can lower this number if the function doesn't work correctly.
+	local ped = PlayerPedId()
+	if IsPedArmed(ped, 6) then
+		isArmed = true
+	else
+		isArmed = false
+	end
+   end
+end)
+			
+
+-- No weapons drop on player's death
 function NoWeapons()
 	local handle, ped = FindFirstPed()
 	local finished = false
